51927af4eff57229f08588be35c718f26e2dee74
Ruterryter <50732755+Ruterryter@users.noreply.github.com>
2020-03-03 18:40:06 +0100

    excerices

--- src/8-hacker-speak.js
+++ src/8-hacker-speak.js
@@ -1,3 +1,12 @@
 export const hackerSpeak = (words) => {
+
+  words = words.replace(a / gi, "4")
+  words = words.replace(a / gi, "3")
+  words = words.replace(a / gi, "1")
+  words = words.replace(a / gi, "0")
+  words = words.replace(a / gi, "5")
+
+
+
   return words
 }
